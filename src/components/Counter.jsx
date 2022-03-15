caff85c9d16a2d3b5bc0cbcce5767fa3b9975b97
Alexey Evseev @stalk <myhappydo@gmail.com>
2022-03-16 02:30:18 +0300

    Increase

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 class Counter extends Component {
 
-  renderSomething() {
-    return <div>Something</div>;
-  }
-
   render() {
     return (
       <div>
